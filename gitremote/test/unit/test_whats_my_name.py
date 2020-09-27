dbaf2f926003a7ec7c45ded0d778d299b0bdf014
Ryan <rpslep@my.waketech.edu>
2020-09-27 15:44:53 -0400

    updated name to test

--- gitremote/test/unit/test_whats_my_name.py
+++ gitremote/test/unit/test_whats_my_name.py
@@ -4,4 +4,4 @@ from gitremote.whats_my_name import my_name_is
 
 
 def test_my_name_is():
-    assert "rpslep@my.waketech.edu" == my_name_is()
+    assert "Eugene Zuckerberg" == my_name_is()
--- gitremote/whats_my_name.py
+++ gitremote/whats_my_name.py
@@ -1,5 +1,5 @@
 def my_name_is():
-    return "rpslep@my.waketech.edu"
+    return "Eugene Zuckerberg"
 
 
 # palaheel76 doesn't want any comments
